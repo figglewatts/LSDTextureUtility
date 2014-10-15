e0cbf6481f6b8a91a515b1582f0ea8cff3d5a372
Figglewatts <figglewatts@hotmail.co.uk>
2014-10-15 15:24:28 +0100

    Added basic JFrame

--- src/figglewatts/tools/LSDTextureUtility/LSDTextureUtility.java
+++ src/figglewatts/tools/LSDTextureUtility/LSDTextureUtility.java
@@ -1,10 +1,17 @@
 package figglewatts.tools.LSDTextureUtility;
 
-public class LSDTextureUtility {
+import javax.swing.JFrame;
+
+public class LSDTextureUtility extends JFrame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new LSDTextureUtility();
+	}
+	
+	public LSDTextureUtility() {
+		this.setSize(400, 300);
+		setVisible(true);
 	}
 
 }
